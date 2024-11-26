a4279e48fefc09b71f9ed46ae7b626903441d8ae
JoaquinBL16 <84550387+JoaquinBL16@users.noreply.github.com>
2024-11-26 14:41:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col 
+import requests
 
 # Write directly to the app
 st.title(" Customize your Smoothie ")
@@ -46,6 +47,8 @@ if ingredients_list:
         st.write(my_insert_stmt)
         st.stop()
 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
     #st.write(my_insert_stmt)
     
 
